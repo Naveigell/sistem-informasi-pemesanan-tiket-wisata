eada2951282fefd58d0bdaf928386c3c1f69fb2d
Naveigell <yosua.yerikho123@gmail.com>
2024-06-23 20:47:09 +0800

    feature(maps): add maps in home page

--- resources/views/customer/pages/home/index.blade.php
+++ resources/views/customer/pages/home/index.blade.php
@@ -29,7 +29,7 @@
 
             <br>
 
-            <h2 class="h2 mt-5 text-center">Testimonials</h2>
+            <h2 class="h2 mt-5 pt-5 text-center">Testimonials</h2>
 
             <div id="carouselExampleControls" class="carousel slide mt-5 pt-5" data-ride="carousel">
                 <div class="carousel-inner">
@@ -56,6 +56,12 @@
                     <span class="sr-only">Next</span>
                 </a>
             </div>
+
+            <br>
+
+            <h2 class="h2 mt-5 pt-5 mb-5 text-center">Lokasi - <i class="fa fa-map-marker"></i></h2>
+
+            <iframe style="width: 100%; height: 500px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d629.1001889967912!2d115.13624566952565!3d-8.23731505224863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd18f26b36b8abd%3A0x2d54e26d7664901d!2sWanagiri%20Heaven%20Selfie%20Pucak!5e0!3m2!1sid!2sid!4v1719146585400!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
             <br>
 
