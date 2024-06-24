063c7f32f5058409bd2c7c0677602e8c3d7a438c
Naveigell <yosua.yerikho123@gmail.com>
2024-06-24 20:40:47 +0800

    bugfix(customer/menu): fix menu icon not opened

--- public/customer/assets/js/main.js
+++ public/customer/assets/js/main.js
@@ -18,53 +18,53 @@
 		var $form = $('form');
 
 		// Auto-resizing textareas.
-			$form.find('textarea').each(function() {
-
-				var $this = $(this),
-					$wrapper = $('<div class="textarea-wrapper"></div>'),
-					$submits = $this.find('input[type="submit"]');
-
-				$this
-					.wrap($wrapper)
-					.attr('rows', 1)
-					.css('overflow', 'hidden')
-					.css('resize', 'none')
-					.on('keydown', function(event) {
-
-						if (event.keyCode == 13
-						&&	event.ctrlKey) {
-
-							event.preventDefault();
-							event.stopPropagation();
-
-							$(this).blur();
-
-						}
-
-					})
-					.on('blur focus', function() {
-						$this.val($.trim($this.val()));
-					})
-					.on('input blur focus --init', function() {
-
-						$wrapper
-							.css('height', $this.height());
-
-						$this
-							.css('height', 'auto')
-							.css('height', $this.prop('scrollHeight') + 'px');
-
-					})
-					.on('keyup', function(event) {
-
-						if (event.keyCode == 9)
-							$this
-								.select();
-
-					})
-					.triggerHandler('--init');
-
-			});
+		// 	$form.find('textarea').each(function() {
+        //
+		// 		var $this = $(this),
+		// 			$wrapper = $('<div class="textarea-wrapper"></div>'),
+		// 			$submits = $this.find('input[type="submit"]');
+        //
+		// 		$this
+		// 			.wrap($wrapper)
+		// 			.attr('rows', 1)
+		// 			.css('overflow', 'hidden')
+		// 			.css('resize', 'none')
+		// 			.on('keydown', function(event) {
+        //
+		// 				if (event.keyCode == 13
+		// 				&&	event.ctrlKey) {
+        //
+		// 					event.preventDefault();
+		// 					event.stopPropagation();
+        //
+		// 					$(this).blur();
+        //
+		// 				}
+        //
+		// 			})
+		// 			.on('blur focus', function() {
+		// 				$this.val($.trim($this.val()));
+		// 			})
+		// 			.on('input blur focus --init', function() {
+        //
+		// 				$wrapper
+		// 					.css('height', $this.height());
+        //
+		// 				$this
+		// 					.css('height', 'auto')
+		// 					.css('height', $this.prop('scrollHeight') + 'px');
+        //
+		// 			})
+		// 			.on('keyup', function(event) {
+        //
+		// 				if (event.keyCode == 9)
+		// 					$this
+		// 						.select();
+        //
+		// 			})
+		// 			.triggerHandler('--init');
+        //
+		// 	});
 
 	// Menu.
 		var $menu = $('#menu');
--- resources/views/layouts/customer/script.blade.php
+++ resources/views/layouts/customer/script.blade.php
@@ -4,7 +4,7 @@
 <script src="{{ asset('customer/assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('customer/assets/js/jquery.scrolly.min.js') }}"></script>
 <script src="{{ asset('customer/assets/js/jquery.scrollex.min.js') }}"></script>
-{{--<script src="{{ asset('customer/assets/js/main.js') }}"></script>--}}
+<script src="{{ asset('customer/assets/js/main.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js" integrity="sha512-uURl+ZXMBrF4AwGaWmEetzrd+J5/8NRkWAvJx5sbPSSuOb0bZLqf+tOzniObO00BjHa/dD7gub9oCGMLPQHtQA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
     $("a.image-zoom").fancybox({
