3dad266cdc0126ae48c83384b57c0097ca6c3ce4
Naveigell <yosua.yerikho123@gmail.com>
2024-07-03 09:50:37 +0800

    style(customer/menu): add active and unactive in customer menu

--- resources/views/layouts/customer/header.blade.php
+++ resources/views/layouts/customer/header.blade.php
@@ -21,12 +21,13 @@
 <nav id="menu">
     <h2>Menu</h2>
     <ul>
-        <li><a href="{{ route('home.index') }}" class="active">Home</a></li>
+        <li><a href="{{ route('home.index') }}" @if(request()->routeIs('home.index')) class="active" @endif>Home</a></li>
 
-        <li><a href="{{ route('tickets.index') }}">Pesan Tiket</a></li>
-        <li><a href="{{ route('galleries.index') }}">Galeri</a></li>
+        <li><a href="{{ route('tickets.index') }}" @if(request()->routeIs('tickets.index')) class="active" @endif>Pesan Tiket</a></li>
+        <li><a href="{{ route('galleries.index') }}" @if(request()->routeIs('galleries.index')) class="active" @endif>Galeri</a></li>
         @if(optional(auth()->user())->isCustomer())
-            <li><a href="{{ route('profile.create') }}">Profile &nbsp; <i class="fa fa-user"></i></a></li>
+            <li><a href="{{ route('customer.transactions.index') }}" @if(request()->routeIs('customer.transactions.index')) class="active" @endif>Transaksi &nbsp; <i class="fa fa-money"></i></a></li>
+            <li><a href="{{ route('profile.create') }}" @if(request()->routeIs('profile.create')) class="active" @endif>Profile &nbsp; <i class="fa fa-user"></i></a></li>
         @endif
         @if(optional(auth()->user())->isCustomer())
             <li><a href="{{ route('auth.logout') }}">Logout</a></li>
